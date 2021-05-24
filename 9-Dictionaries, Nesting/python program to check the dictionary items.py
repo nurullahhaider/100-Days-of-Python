20effd30d0f0ff58bc1a55e3a28ef7345e2bad58
nurullahhaider <82478841+nurullahhaider@users.noreply.github.com>
2021-05-24 20:37:47 +0530

    Add files via upload
    python program to check the dictionary items

---
+++ 9-Dictionaries, Nesting/python program to check the dictionary items.py
@@ -0,0 +1,9 @@
+# python code to check the dictionary items
+dict= { "bug":"bug value",
+        "function":"a piece of code",
+        "loop":"action loop",
+      }
+#print(dict)
+for key in dict:
+    print ("key= ",key)
+    print ("value= ",dict[key])
