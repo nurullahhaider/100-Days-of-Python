d579737fa9a7263feecf9015f31fbdab762c4f83
nurullahhaider <82478841+nurullahhaider@users.noreply.github.com>
2021-05-25 22:53:29 +0530

    Create swichting the values of 2 variables-short.py

---
+++ 1 variables/swichting the values of 2 variables-short.py
@@ -0,0 +1,9 @@
+# w2 swichting the values of 2 variables-short method
+first_number= input("first number= ")
+second_number= input("second number= ")
+
+first_number, second_number= second_number, first_number
+
+print("after switching")
+print("first number= ",first_number)
+print("second number= ",second_number)
